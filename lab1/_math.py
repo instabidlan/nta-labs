b914c7528ce17b899cd951847e7f9e854da4f03c
instabidlan <vlad.tsema76@gmail.com>
2024-03-15 04:27:04 +0200

    Trial division and Miller-Rabin tests

---
+++ lab1/_math.py
@@ -0,0 +1,42 @@
+from random import randrange
+from math import gcd, sqrt
+
+
+def testTrialDiv(p: int, threshold=0) -> bool:
+    for d, i in enumerate(range(1, int(sqrt(p)))):
+        if p % d == 0:
+            return False
+        
+        if threshold and i == threshold:
+            return False
+        
+    return True
+
+
+def testMillerRabin(p: int, k: int) -> bool:
+    s, d = 0, p - 1
+    while d % 2 == 0:
+        d //= 2; s += 1
+
+    for _ in range(k):
+        isStrongPseudoprime = False
+        x = randrange(2, p, 1)
+
+        if gcd(x, p) > 1:
+            return False
+        
+        if (xd := pow(x, d, p)) == 1 or xd == p - 1:
+            continue
+        else:
+            for _ in range(1, s):
+                if (xd := pow(xd, 2, p)) == p - 1:
+                    isStrongPseudoprime = True
+                    break
+                elif xd == 1:
+                    return False
+            
+            if isStrongPseudoprime == False:
+                return False
+            
+    return True
+
